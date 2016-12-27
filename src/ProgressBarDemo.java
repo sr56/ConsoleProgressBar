e037c07cb4a47e1022308022dfa27a96dee9f818
Suraj <r.suraj@habibbank.com>
2016-12-27 14:35:42 +0400

    Initial commit

---
+++ src/ProgressBarDemo.java
@@ -0,0 +1,56 @@
+/**
+ * Created by suraj on 12/27/16.
+ */
+
+
+public class ProgressBarDemo {
+    /*ANSI color code Escape sequences. */
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+
+    public static void startDemo() {
+        int completed = 0;
+        char over = '▓', pending = '░', working = '▒';
+        String print = "", output = "";
+        String[] wheel = {"◜", "◝", "◞", "◟", "◯"};
+
+        while (completed++ <= 100) {
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+            }
+
+            output = "Working.";
+            for (int i = 0; i <= 5; i++) {
+                print = ((completed % 5) <= i) ? " " : ".";
+                output += print;
+            }
+            output += " " + PURPLE + wheel[(completed % wheel.length)] + RESET + " [";
+
+            for (int i = 0; i <= 100; i++) {
+                if (i < completed) {
+                    print = GREEN + over + RESET;
+                } else if (i == completed) {
+                    print = BLUE + working + RESET;
+                } else {
+                    print = RED + pending + RESET;
+                }
+                output += print;
+            }
+            output += " ] " + (completed - 1) + "%   ";
+
+            System.out.print("\r");
+            System.out.print(output);
+        }
+        System.out.println(" Done!");
+    }
+
+    public static void main(String[] args) {
+        startDemo();
+    }
+
+}
